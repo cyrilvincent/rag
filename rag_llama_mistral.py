4c15ef4398dabf55d6331fb197cc4396d54a7d05
Cyril Vincent <contact@cyrilvincent.com>
2025-06-14 20:34:17 +0200

    Agent

--- rag_llama_mistral.py
+++ rag_llama_mistral.py
@@ -1,7 +1,10 @@
 import os
 from llama_index.core import Document, GPTVectorStoreIndex, Settings
+from llama_index.core.schema import NodeRelationship
 from llama_index.embeddings.mistralai import MistralAIEmbedding
 from llama_index.llms.mistralai import MistralAI
+import numpy as np
+
 # pip install llama-index-embeddings-mistralai
 # pip install llama-index-llms-mistralai
 
@@ -16,26 +19,49 @@ print(f"Dimension of embeddings: {len(embeddings)}")
 Settings.embed_model = embed_model
 files = [f.path for f in os.scandir("data/books")]
 
+def extract_nodes(vector_store):
+    node_dict = {}
+    for node_id, metadata in vector_store._data.metadata_dict.items():
+        node_dict[node_id] = metadata
+    return node_dict
+
+
 documents = []
 for file in files:
     print(f"Loading {file}")
     with open(file, encoding="utf-8") as f:
-        documents.append(Document(text=f.read()))
+        documents.append(Document(text=file + "\n" + f.read()))
 
 llm=MistralAI(api_key=api_key)
 print("Embedding")
 index = GPTVectorStoreIndex.from_documents(documents, show_progress=True, llm=llm)
 
+vector_store = index.storage_context.vector_store
+vector_store_dict = vector_store.to_dict()
+keys = extract_nodes(vector_store)
+
+
 query_engine = index.as_query_engine(similarity_top_k=3, llm=llm)
-# query = "How old was Leonardo when he died?"
+query = "How old was Leonardo when he died?"
 # query = "When Leonardo leave Milan ?"
 # query = "Dessine moi un mouton"
-query = "Quand est né Cyril et qui sont ces enfants ?"
+# query = "Quand est né Cyril et qui sont ces enfants ?"
 response = query_engine.query(query)
 print(response)
 retriever = index.as_retriever()
 nodes = retriever.retrieve(query)
 print(nodes)
+for node in nodes:
+    print(f"Node {node.id_}")
+    print(f"Score {node.score}")
+    # source_id = node.node.relationships[NodeRelationship.SOURCE].node_id
+    # print(f"Source: {source_id}")
+    document_id = keys[node.id_]["document_id"]
+    print(f"Document id {document_id}")
+    document = [d for d in documents if d.id_ == document_id][0]
+    print(f"From document {document.text.split("\n")[0]}")
+    print(node.text[:255])
+    print()
 
 
 
--- wiki_scraper.py
+++ wiki_scraper.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+from googlesearch import search
+# pip install googlesearch-python
 
 class WikiScraper:
 
     def __init__(self, url: str, destination="data/wiki/"):
         self.url = url
-        self.destination = destination + url[self.url.rindex("/"):] + ".txt"
+        self.destination = destination + url[self.url.rindex("/"):].replace(":", "") + ".txt"
         self.paragraphes = []
 
     def scrap(self):
@@ -18,8 +20,6 @@ class WikiScraper:
             paragraphe = node.get_text().strip()
             sentences = paragraphe.split("\n")
             for sentence in sentences:
-                if "{\\displaystyle" in sentence:
-                    pass
                 if len(sentence) > 1:
                     if sentence[0] != "{":
                         sentence = self.replace(sentence)
@@ -30,20 +30,34 @@ class WikiScraper:
         s = s.replace(" ", " ")
         return s
 
-
     def save(self):
         with open(self.destination, "w", encoding="utf-8") as f:
             for paragraphe in self.paragraphes:
                 f.write(paragraphe + "\n")
 
+    @staticmethod
+    def search_and_scrap(q: str, destination="data/wiki/", num_results=10, lang="fr"):
+        results = search(q, lang=lang, num_results=num_results, unique=True)
+        for result in results:
+            if "wikipedia.org" in result:
+                try:
+                    scraper = WikiScraper(result, destination=destination)
+                    scraper.scrap()
+                    scraper.save()
+                except Exception as ex:
+                    print(f"Error {ex}")
+
+
+
 
 if __name__ == '__main__':
-    url = "https://fr.wikipedia.org/wiki/Grand_mod%C3%A8le_de_langage"
-    scrap = WikiScraper(url)
-    scrap.scrap()
-    scrap.save()
-    url = "https://fr.wikipedia.org/wiki/G%C3%A9n%C3%A9ration_augment%C3%A9e_de_r%C3%A9cup%C3%A9ration"
-    scrap = WikiScraper(url)
-    scrap.scrap()
-    scrap.save()
+    # url = "https://fr.wikipedia.org/wiki/Grand_mod%C3%A8le_de_langage"
+    # scrap = WikiScraper(url)
+    # scrap.scrap()
+    # scrap.save()
+    # url = "https://fr.wikipedia.org/wiki/G%C3%A9n%C3%A9ration_augment%C3%A9e_de_r%C3%A9cup%C3%A9ration"
+    # scrap = WikiScraper(url)
+    # scrap.scrap()
+    # scrap.save()
+    WikiScraper.search_and_scrap("wikipedia large language model", num_results=100)
 
